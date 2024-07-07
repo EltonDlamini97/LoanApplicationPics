f15c02a2ba2beefc88be175b6189ec590e7ca045
EltonDlamini97 <106653859+EltonDlamini97@users.noreply.github.com>
2024-07-07 22:35:12 +0000

    Loan Application Pictures

--- import pandas as pd # type: ignore.py
+++ import pandas as pd # type: ignore.py
@@ -6,11 +6,11 @@ dataset_path = 'Images/ProjectFinalResult.csv'
 df = pd.read_csv(dataset_path, delimiter=';')
 
 # Define the base URL
-base_url = 'https://github.com/EltonDlamini97/LoanApplicationPics.git'
+base_url = 'https://raw.githubusercontent.com/EltonDlamini97/LoanApplicationPics/main/'
 
 # Generate the URLs
 df['Image_URL'] = df['Images'].apply(lambda x: base_url + os.path.basename(x))
 
 # Save the updated dataset
-df.to_csv('Images/ProjectFinalResult_with_URLs.csv', index=False)
+df.to_csv('LoanApplicationPic/ProjectFinalResult_URLs.csv', index=False)
 
--- import pandas as pd.py
+++ import pandas as pd.py
@@ -7,25 +7,27 @@ dataset_path = 'Images/ProjectFinalResult.csv'
 df = pd.read_csv(dataset_path, delimiter=';')
 
 # Define the base URL
-base_url = 'https://github.com/EltonDlamini97/LoanApplicationPics/raw/main/'
+base_url = 'https://raw.githubusercontent.com/EltonDlamini97/LoanApplicationPics/main/'
 
 # Generate the URLs
 df['Image_URL'] = df['Images'].apply(lambda x: base_url + os.path.basename(x))
 
+# Define the updated dataset path
+updated_dataset_path = 'LoanApplicationPic/ProjectFinalResult_URLs.csv'
+
 # Save the updated dataset
-updated_dataset_path = 'Images/ProjectFinalResult_with_URLs.csv'
 df.to_csv(updated_dataset_path, index=False)
 
-# Define the local repository path
-repo_path = 'LoanApplicationPics'
+# Git commands to add, commit, and push the updated CSV to the repository
+repo_path = 'LoanApplicationPics'  # Change this to your repository's path
 
-# Copy the updated dataset to the repository
-os.replace(updated_dataset_path, os.path.join(repo_path, 'Images/ProjectFinalResult_with_URLs.csv'))
+# Move the updated dataset to the repository directory
+os.replace(updated_dataset_path, os.path.join(repo_path, updated_dataset_path))
 
 # Change directory to the repository
 os.chdir(repo_path)
 
-# Run Git commands to push the changes to GitHub
-subprocess.run(['git', 'add', 'Images/ProjectFinalResult_with_URLs.csv'])
+# Run Git commands to push the changes
+subprocess.run(['git', 'add', updated_dataset_path])
 subprocess.run(['git', 'commit', '-m', 'Add ProjectFinalResult with image URLs'])
 subprocess.run(['git', 'push', 'origin', 'main'])
